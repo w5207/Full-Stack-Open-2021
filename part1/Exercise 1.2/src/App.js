ac76745cf122c97f884e8d2221c43c188a6d7e04
Windows-PC\Windows <n5207w@gmail.com>
2021-06-09 00:17:21 -0400

    no message

---
+++ part1/Exercise 1.2/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Header = (props) => {
+  return <div>{props.course}</div>;
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part}
+        {props.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = () => {
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
